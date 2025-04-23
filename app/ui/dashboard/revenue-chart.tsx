3904c7b48486f6fe18c0807d27dc29d5141102a7
yegri <exsilver305@gmail.com>
2025-04-23 17:59:43 +0900

    feat: 컴포넌트별 skeleton 적용

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -9,12 +9,8 @@ import { fetchRevenue } from '@/app/lib/data';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
-  // const revenue = await fetchRevenue()
+export default async function RevenueChart() {
+  const revenue = await fetchRevenue()
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
